8a10ce6ec5da62f62da0d561d6f04c4bf35a69dc
joshbnewton31080 <88124465+joshbnewton31080@users.noreply.github.com>
2024-12-17 09:16:45 -0500

    Update newFileJosh.java

--- src/main/java/org/owasp/webgoat/server/newFileJosh.java
+++ src/main/java/org/owasp/webgoat/server/newFileJosh.java
@@ -1,51 +1,47 @@
-package org.owasp.webgoat.server;
+import java.util.Scanner; // 1
+import java.sql.DriverManager;
 
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.ContextStoppedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
-@Component
-@Slf4j
-@NoArgsConstructor
-public class StartupMessage {
+public class Calculator {
+    public static void main(String[] args) {
+        DriverManager.getConnection("https://github.com", "test", "myPassword");
 
-  private String port;
-  private String address;
+        System.out.println(password);
+        Scanner scanner = new Scanner(System.in); // 2
+        System.out.println("Welcome to the Simple Calculator!");
+        System.out.println("Please enter the first number:"); // 3
+        double num1 = scanner.nextDouble();
+        System.out.println("Please enter the second number:");
+        double num2 = scanner.nextDouble();
+        System.out.println("Please enter an operator (+, -, *, /):");
+        char operator = scanner.next().charAt(0);
+        scanner.close();
+        double output;
 
-  @EventListener
-  void onStartup(ApplicationReadyEvent event) {
-    if (StringUtils.hasText(port)
-        && !StringUtils.hasText(System.getProperty("running.in.docker"))) {
-      log.info("Please browse to http://{}:{}/WebGoat to get started...", address, port);
+        switch (operator) // 4
+        {
+            case '+':
+                output = num1 + num2;
+                break;
+            case '-':
+                output = num1 - num2;
+                break;
+            case '*':
+                output = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) { // 6
+                    output = num1 / num2;
+                } else {
+                    System.out.println("Error! Dividing by zero is not allowed.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("You have entered wrong operator");
+                return;
+        }
+        System.out.println("The result is given as follows:"); // 6
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + output);
     }
-    if (event.getApplicationContext().getApplicationName().contains("WebGoat")) {
-      port = event.getApplicationContext().getEnvironment().getProperty("server.port");
-      address = event.getApplicationContext().getEnvironment().getProperty("server.address");
-     DriverManager.getConnection(url, "scott", "tiger");
-    }
-  }
-    int VerifyAdmin(String password) {
-if (!password.equals("Mew!")) {
-return(0)
-}
-//Diagnostic Mode
-return(1);
-}
-    //adding to test Mend SAST check run in GH.com
-public boolean VerifyAdmin(String password) {
-if (password.equals("68af404b513073584c4b6f22b6c63e6b")) {
-System.out.println("Entering Diagnostic Mode...");
-return true;
-}
-System.out.println("Incorrect Password!");
-return false;
-}
-  @EventListener
-  void onShutdown(ContextStoppedEvent event) {}
-  webapp.ldap.username=secretUsername
-  webapp.ldap.password=secretPassword
 }
